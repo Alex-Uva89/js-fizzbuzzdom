7897f75aa2c5b5f5add00be41a73ef53ce801c2a
ALESSANDROUVA <a.uva1988@gmail.com>
2022-02-23 17:37:29 +0100

    Ultimate effect

--- js/script.js
+++ js/script.js
@@ -3,18 +3,20 @@ let areaContainBoxes = document.querySelector('.area-boxes');
 
 // input
 for (let i = 1; i <= 100; i++) {
-    if (i% 15 == 0) {
+    
+    if (i % 3 == 0 && i % 5 == 0) {
         areaContainBoxes.innerHTML += `<div class="box orange">BuzFizz</div>`
     } else if (i% 3 == 0){
         areaContainBoxes.innerHTML += `<div class="box yellow">Buz</div>`
     } else if (i% 5 == 0) {
         areaContainBoxes.innerHTML += `<div class="box pink">Fizz</div>`    
-    } 
-    
-    else{
+    } else {
         areaContainBoxes.innerHTML += `<div class="box">${i}</div>`
     }
+
 }
+
+
 
 let caselBox = document.querySelectorAll('.box');
 
